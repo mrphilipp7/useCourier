03d8b9a1ce42966a4b527eb448065eb2f571366c
Zach Philipp <zachphilipp@icloud.com>
2026-09-23 11:28:14 -0500

    Updates

---
+++ documentation/.vitepress/config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "useCourier",
+  description: "A React hook for helping track file uploads",
+  base: "/useCourier/",
+  themeConfig: {
+    sidebar: [
+      {
+        text: "Introduction",
+        items: [{ text: "Getting Started", link: "/get-started" }],
+      },
+      {
+        text: "API Reference",
+        items: [
+          { text: "Functions and Options", link: "/api" },
+          { text: "Large File Uploads", link: "/large-files" },
+          { text: "Backend Integration", link: "/backend-integration" },
+        ],
+      },
+      {
+        text: "Framework Examples",
+        items: [
+          { text: "Express", link: "/examples/express" },
+          { text: "Next.js", link: "/examples/nextjs" },
+          { text: "TanStack Start", link: "/examples/tanstack-start" },
+          { text: "Hono", link: "/examples/hono" },
+        ],
+      },
+      {
+        text: "Integrations",
+        items: [
+          {
+            text: "Shadcn Attachments",
+            link: "/integrations/shadcn-attachments",
+          },
+        ],
+      },
+      {
+        text: "Other",
+        items: [
+          { text: "About", link: "/about" },
+          { text: "License", link: "/license" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/mrphilipp7/useCourier" },
+    ],
+  },
+});
